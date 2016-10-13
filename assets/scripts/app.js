ea458a6422ef8aa38296c1bb42fa939d48475ed5
dvdpwll <dvdpwll456@gmail.com>
2016-10-13 15:22:42 -0400

    deploy

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,7 @@
 
 const app = {
   // api: 'http://localhost:3000',
-  api: 'https://sheltered-mountain-36847.herokuapp.com/',
+  api: 'https://sheltered-mountain-36847.herokuapp.com',
   currentPath: '',
 };
 
